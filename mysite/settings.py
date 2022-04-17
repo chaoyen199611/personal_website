fec5254e206b0aea004b18bd8660bed63e1e9ffd
Chao-Yen Huang <chaoyen510220@gmail.com>
2022-04-17 22:52:35 +0800

    add slider

--- mysite/settings.py
+++ mysite/settings.py
@@ -124,7 +124,7 @@ USE_TZ = False
 STATIC_URL = 'static/'
 
 STATICFILES_DIRS=[
-    os.path.join(BASE_DIR,'templates/static'),
+    os.path.join(BASE_DIR,"static"),
 ]
 
 # Default primary key field type
